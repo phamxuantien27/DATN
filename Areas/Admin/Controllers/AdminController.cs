b49e72031bb9de3218cef7895d03fa23c16a4318
namvgt <namvgt75@gmail.com>
2022-07-06 10:28:15 +0700

    first commit

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Update.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Phanmem()
+        {
+            return View();
+        }
+        public ActionResult Maytinh()
+        {
+            return View();
+        }
+        public ActionResult CVE()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Update.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Phanmem()
+        {
+            return View();
+        }
+        public ActionResult Luutru()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(string username, string pwd)
+        {
+            return View("Index");
+        }
+    }
+}
